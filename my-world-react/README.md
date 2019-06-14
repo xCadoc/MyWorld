29a15fd38bcae540fc320162dda1fe0709542965
xCadoc <azura.deghan@gmail.com>
2019-06-14 23:01:42 +0200

    Initial react setup

---
+++ my-world-react/README.md
@@ -0,0 +1,10 @@
+## Start application
+
+run ./start.bat or npm run start in command line
+start spring boot project via Run -> Application
+check if mySql is running
+
+
+## Access application
+react application see run script output
+spring boot is currently only localhost:8080
--- myworld/src/main/java/myworld/controller/MainController.java
+++ myworld/src/main/java/myworld/controller/MainController.java
@@ -5,9 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import myworld.model.Book;
 import myworld.service.BookService;
 
 @Controller
