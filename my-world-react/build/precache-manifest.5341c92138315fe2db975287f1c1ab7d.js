2b044bca4ba23a1f363f3d8d0a702bb5270e1483
xCadoc <azura.deghan@gmail.com>
2019-12-27 23:08:08 +0100

    production build

---
+++ my-world-react/build/precache-manifest.5341c92138315fe2db975287f1c1ab7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c8f8f74939763e3cd32",
+    "url": "/static/css/main.0e4ccfc4.chunk.css"
+  },
+  {
+    "revision": "4c8f8f74939763e3cd32",
+    "url": "/static/js/main.057f4549.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "900bbb1b0fcc9823d873",
+    "url": "/static/css/2.08244895.chunk.css"
+  },
+  {
+    "revision": "900bbb1b0fcc9823d873",
+    "url": "/static/js/2.5cd80a93.chunk.js"
+  },
+  {
+    "revision": "20615d7746a684f94331f1ef90a392da",
+    "url": "/static/media/eragon_1.20615d77.jpg"
+  },
+  {
+    "revision": "2777382f5229810b5d5f660b94b47485",
+    "url": "/static/media/background-grey.2777382f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cefb713781496dec4f5128367c932ff5",
+    "url": "/static/media/background_paper_1.cefb7137.png"
+  },
+  {
+    "revision": "f401eb0f2daf019470639c60599e013f",
+    "url": "/index.html"
+  }
+];
