02ecfd29004402ddfe9e884f4881d83191534c1a
xCadoc <azura.deghan@gmail.com>
2019-12-27 22:45:13 +0100

    change book sql

--- myworld/src/main/scripts/book.sql
+++ myworld/src/main/scripts/book.sql
@@ -1,4 +1,5 @@
 
+/* local */
 DROP TABLE IF EXISTS `myworlddb`.`book`;
 
 CREATE TABLE `myworlddb`.`book` (
@@ -8,3 +9,17 @@ CREATE TABLE `myworlddb`.`book` (
   `created_at` DATETIME NULL,
   `changed_at` DATETIME NULL,
   PRIMARY KEY (`id`));
+  
+  
+/* remote */
+DROP TABLE IF EXISTS `sql7317052`.`book`;
+
+CREATE TABLE `sql7317052`.`book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(200) NULL,
+  `created_at` DATETIME NULL,
+  `changed_at` DATETIME NULL,
+  PRIMARY KEY (`id`));
+
+
