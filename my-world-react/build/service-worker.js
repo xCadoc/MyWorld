077485e5489edde985d9a7e41fcfcc543e6c9dc2
xCadoc <azura.deghan@gmail.com>
2019-12-27 23:21:21 +0100

    rebuild production code

--- my-world-react/build/service-worker.js
+++ my-world-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5341c92138315fe2db975287f1c1ab7d.js"
+  "/xCadoc/MyWorld/tree/master/my-world-react/precache-manifest.d7ac27e66ccd1125bbe26c59ee7f0532.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/xCadoc/MyWorld/tree/master/my-world-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
